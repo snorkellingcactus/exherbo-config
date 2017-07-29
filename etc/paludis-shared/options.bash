22df20ea48eac218686f96e2bfada09e25a29de1
Nadal Gonzalo García Zavala <snorkellingcactus@gmail.com>
2017-07-29 17:22:07 -0300

    Convenient line movement

--- etc/paludis-shared/options.bash
+++ etc/paludis-shared/options.bash
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo '
-	*/* -man build_options: symbols=strip jobs=9 LIBC: glibc -musl parts: -documentation
+	*/* -man -development -readline build_options: symbols=strip -recommended_tests dwarf_compress jobs=9 LIBC: glibc -musl parts: -documentation
 	*/* targets: x86_64-pc-linux-gnu i686-pc-linux-gnu
 	*/* -systemd consolekit
 	*/* providers: -systemd eudev rsyslog
@@ -10,14 +10,17 @@ echo '
 	*/* mmx sse sse2 sse3 ssse3 X86_CPU_FEATURES: mmx sse sse2 sse3 ssse3 AMD64_CPU_FEATURES: mmx sse sse2 sse3 ssse3
 	*/* avahi X abi_x86_64 -branding -bash-completion -vim-syntax
 	*/* gstreamer_plugins: ogg cdda theora vorbis libvisual xv pango alsa flac jack pulseaudio taglib speex vpx wavpack musepack resin sndfile faac faad mpg123 cdio dvdread h264 lame mad mpeg2 mp2 mjpeg
-	#*/* gles1 gles2 opengl openal xa xvmc osmesa g3dvl drm gallium openvg
+	*/* nsplugin
 	*/* LINGUAS:		-* es_LA es_AR es_ES es es_ar es-es es-la
+	*/* INPUT_DEVICES:	-* evdev keyboard synaptics
 	*/* VIDEO_CARDS:	-* intel
 	*/* ALSA_CARDS:		-* hda-intel
-	*/* INPUT_DEVICES:	-* evdev keyboard synaptics
-	#*/* qt5
-	*/* nsplugin
-	*/* Build_Options: -recommended_tests
+
+	#Para webkit-2.11?
+	dev-lang/llvm static
+
+	app-text/poppler lcms cairo
+	net-dns/avahi dbus dns_sd
 
 	#Requerido por la constraint opencl de mesa i915
 	#xa xvmc
@@ -37,12 +40,6 @@ echo '
 	#sys-devel/gcc -threads
 
 	sys-devel/gcc graphite
-
-	#Para webkit-2.11?
-	dev-lang/llvm static
-
-	app-text/poppler lcms cairo
-	net-dns/avahi dbus dns_sd
 
 	#Para git
 	dev-lang/swig perl
